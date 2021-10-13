c7a529e712ea46ceddae4f02b0e1a8d118220ddf
Ahmad Kholid <kholid060@gmail.com>
2021-10-13 20:01:05 +0800

    feat: add comparison handler

---
+++ src/utils/compare-block-value.js
@@ -0,0 +1,16 @@
+const handlers = {
+  '==': (a, b) => a === b,
+  '>': (a, b) => a > b,
+  '>=': (a, b) => a >= b,
+  '<': (a, b) => a < b,
+  '<=': (a, b) => a <= b,
+  '()': (a, b) => a?.includes(b) ?? false,
+};
+
+export default function (type, valueA, valueB) {
+  const handler = handlers[type];
+
+  if (handler) return handler(valueA, valueB);
+
+  return false;
+}
