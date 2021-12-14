9adb2a6ad47b891df7f01c7e0f39be20e696fb6a
Jérémy Lopes <dev.jlopes@gmail.com>
2021-12-14 19:18:04 +0100

    Add fr locale

--- src/lib/dayjs.js
+++ src/lib/dayjs.js
@@ -3,6 +3,7 @@ import relativeTime from 'dayjs/plugin/relativeTime';
 import 'dayjs/locale/zh';
 import 'dayjs/locale/zh-tw';
 import 'dayjs/locale/vi';
+import 'dayjs/locale/fr';
 
 dayjs.extend(relativeTime);
 
