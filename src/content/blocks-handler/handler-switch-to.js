53f59f101782c6ffcd366659af81d55c3d8cec6a
Ahmad Kholid <Kholid060@gmail.com>
2021-12-22 12:25:29 +0800

    feat: support contenteditable element when using the forms block

--- src/content/blocks-handler/handler-switch-to.js
+++ src/content/blocks-handler/handler-switch-to.js
@@ -4,7 +4,6 @@ function switchTo(block) {
   return new Promise((resolve, reject) => {
     handleElement(block, {
       onSelected(element) {
-        console.log(element, element.tagName);
         if (element.tagName !== 'IFRAME') {
           reject(new Error('not-iframe'));
           return;
