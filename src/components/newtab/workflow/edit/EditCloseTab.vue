36f4336fc31a51ae90bfa3bdfb298e9c9bae611e
Ahmad Kholid <Kholid060@gmail.com>
2022-01-09 16:54:17 +0800

    feat: add close window option in close tab block

--- src/components/newtab/workflow/edit/EditCloseTab.vue
+++ src/components/newtab/workflow/edit/EditCloseTab.vue
@@ -1,31 +1,55 @@
 <template>
   <div class="mb-2 mt-4">
-    <div class="mb-2">
-      <ui-checkbox
-        :model-value="data.activeTab"
-        @change="updateData({ activeTab: $event })"
-      >
-        {{ t('workflow.blocks.close-tab.activeTab') }}
-      </ui-checkbox>
-    </div>
-    <ui-input
-      v-if="!data.activeTab"
-      :model-value="data.url"
-      placeholder="http://example.com/*"
-      @change="updateData({ url: $event })"
+    <ui-select
+      :model-value="data.closeType"
+      placeholder="Close"
+      class="w-full mb-4"
+      @change="updateData({ closeType: $event })"
     >
-      <template #label>
-        {{ t('workflow.blocks.close-tab.url') }}
-        <a
-          href="https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Match_patterns"
-          target="_blank"
-          rel="noopener"
-          title="More info"
+      <option
+        v-for="type in types"
+        :key="type"
+        :value="type"
+        class="capitalize"
+      >
+        {{ type }}
+      </option>
+    </ui-select>
+    <template v-if="data.closeType === 'tab'">
+      <div class="mb-2">
+        <ui-checkbox
+          :model-value="data.activeTab"
+          @change="updateData({ activeTab: $event })"
         >
-          &#9432;
-        </a>
-      </template>
-    </ui-input>
+          {{ t('workflow.blocks.close-tab.activeTab') }}
+        </ui-checkbox>
+      </div>
+      <ui-input
+        v-if="!data.activeTab"
+        :model-value="data.url"
+        placeholder="http://example.com/*"
+        @change="updateData({ url: $event })"
+      >
+        <template #label>
+          {{ t('workflow.blocks.close-tab.url') }}
+          <a
+            href="https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Match_patterns"
+            target="_blank"
+            rel="noopener"
+            title="More info"
+          >
+            &#9432;
+          </a>
+        </template>
+      </ui-input>
+    </template>
+    <ui-checkbox
+      v-else
+      :model-value="data.allWindows"
+      @change="updateData({ allWindows: $event })"
+    >
+      {{ t('workflow.blocks.close-tab.allWindows') }}
+    </ui-checkbox>
   </div>
 </template>
 <script setup>
@@ -40,6 +64,7 @@ const props = defineProps({
 const emit = defineEmits(['update:data']);
 
 const { t } = useI18n();
+const types = ['tab', 'window'];
 
 function updateData(value) {
   emit('update:data', { ...props.data, ...value });
