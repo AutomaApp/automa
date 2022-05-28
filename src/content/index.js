2e58312319f31349fce0e1a6a58a9d0438e7db6b
Ahmad Kholid <kholid060@gmail.com>
2022-05-28 16:41:24 +0800

    fix: connection to the website closed

--- src/content/index.js
+++ src/content/index.js
@@ -139,26 +139,25 @@ function messageListener({ data, source }) {
     return new Promise((resolve, reject) => {
       if (data.isBlock) {
         executeBlock(data).then(resolve).catch(reject);
-        return;
-      }
+      } else {
+        switch (data.type) {
+          case 'condition-builder':
+            handleTestCondition(data.data)
+              .then((result) => resolve(result))
+              .catch((error) => reject(error));
+            break;
+          case 'content-script-exists':
+            resolve(true);
+            break;
+          case 'automa-element-selector': {
+            const selectorInstance = elementSelectorInstance();
 
-      switch (data.type) {
-        case 'condition-builder':
-          handleTestCondition(data.data)
-            .then((result) => resolve(result))
-            .catch((error) => reject(error));
-          break;
-        case 'content-script-exists':
-          resolve(true);
-          break;
-        case 'automa-element-selector': {
-          const selectorInstance = elementSelectorInstance();
-
-          resolve(selectorInstance);
-          break;
+            resolve(selectorInstance);
+            break;
+          }
+          default:
+            resolve(null);
         }
-        default:
-          resolve(null);
       }
     });
   });
