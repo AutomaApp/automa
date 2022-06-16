63aab7e76079990d46783434ded100f4b4a63f15
Ahmad Kholid <Kholid060@gmail.com>
2022-06-16 14:57:40 +0800

    feat: add csv options in export data block

--- src/utils/shared.js
+++ src/utils/shared.js
@@ -367,6 +367,7 @@ export const tasks = {
       type: 'json',
       description: '',
       variableName: '',
+      csvDelimiter: '',
       addBOMHeader: true,
       onConflict: 'uniquify',
       dataToExport: 'data-columns',
