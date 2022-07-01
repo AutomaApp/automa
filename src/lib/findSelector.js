e9ec233d1a6580a5fe9d22fc018d762759df0ea1
Ahmad Kholid <Kholid060@gmail.com>
2022-07-01 14:49:30 +0800

    feat: include tag in the selector

--- src/lib/findSelector.js
+++ src/lib/findSelector.js
@@ -5,9 +5,27 @@ const ariaAttrs = ['aria-label', 'aria-labelledby'];
 export const finder = finderLib;
 
 export default function (element, options = {}) {
-  return finder(element, {
-    tagName: () => true,
+  let selector = finder(element, {
+    tagName: (name) => {
+      console.log(name);
+      return true;
+    },
     attr: (name, value) => name === 'id' || (ariaAttrs.includes(name) && value),
     ...options,
   });
+  console.log({
+    tagName: (name) => {
+      console.log(name);
+      return true;
+    },
+    attr: (name, value) => name === 'id' || (ariaAttrs.includes(name) && value),
+    ...options,
+  });
+
+  const tag = element.tagName.toLowerCase();
+  if (!selector.startsWith(tag) && !selector.includes(' ')) {
+    selector = `${tag}${selector}`;
+  }
+
+  return selector;
 }
