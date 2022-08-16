e9654687946853490546e62a6d11b0d148251e0d
Ahmad Kholid <Kholid060@gmail.com>
2022-08-16 15:31:18 +0800

    fix: selector settings

--- src/content/elementSelector/getSelectorOptions.js
+++ src/content/elementSelector/getSelectorOptions.js
@@ -1,10 +1,8 @@
 export default function ({ idName, tagName, className, attr, attrNames }) {
-  const attrs = attr ? attrNames.split(',').map((item) => item.trim()) : false;
-
   return {
     idName: () => idName ?? true,
     tagName: () => tagName ?? true,
     className: () => className ?? true,
-    attr: (name) => (attr ? false : attrs.includes(name)),
+    attr: (name) => (attr ? attrNames.includes(name) : name === 'data-testid'),
   };
 }
