080a3fe0b7fa892a6d9805395139b8960df290ae
Ahmad Kholid <Kholid060@gmail.com>
2022-08-29 10:38:15 +0800

    fix: can't exclude element id in the element selector settings

--- src/content/elementSelector/getSelectorOptions.js
+++ src/content/elementSelector/getSelectorOptions.js
@@ -1,8 +1,12 @@
 export default function ({ idName, tagName, className, attr, attrNames }) {
+  const attrs = attr
+    ? attrNames.split(',').map((item) => item.trim())
+    : ['data-testid'];
+
   return {
     idName: () => idName ?? true,
     tagName: () => tagName ?? true,
     className: () => className ?? true,
-    attr: (name) => (attr ? attrNames.includes(name) : name === 'data-testid'),
+    attr: (name) => attrs.includes(name),
   };
 }
