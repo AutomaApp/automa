f823ac6936c5579d6a01b2acef6862b5b512c6e9
bingren <bing.ren@yingdao.com>
2025-03-31 17:02:22 +0800

    feat(workflows): add empty state message for workflows when none are available

--- src/components/newtab/workflows/WorkflowsShared.vue
+++ src/components/newtab/workflows/WorkflowsShared.vue
@@ -1,19 +1,35 @@
 <template>
-  <shared-card
-    v-for="workflow in workflows"
-    :key="workflow.id"
-    :data="workflow"
-    :show-details="false"
-    @execute="RendererWorkflowService.executeWorkflow(workflow)"
-    @click="$router.push(`/workflows/${$event.id}/shared`)"
-  />
+  <div
+    v-if="workflows.length === 0"
+    class="md:flex items-center md:text-left text-center py-12"
+  >
+    <img src="@/assets/svg/alien.svg" class="w-96" />
+    <div class="ml-4">
+      <h1 class="mb-6 max-w-md text-2xl font-semibold">
+        {{ t('message.empty') }}
+      </h1>
+    </div>
+  </div>
+  <div v-else class="workflows-container">
+    <shared-card
+      v-for="workflow in workflows"
+      :key="workflow.id"
+      :data="workflow"
+      :show-details="false"
+      @execute="RendererWorkflowService.executeWorkflow(workflow)"
+      @click="$router.push(`/workflows/${$event.id}/shared`)"
+    />
+  </div>
 </template>
 <script setup>
-import { computed } from 'vue';
-import { useSharedWorkflowStore } from '@/stores/sharedWorkflow';
-import { arraySorter } from '@/utils/helper';
 import SharedCard from '@/components/newtab/shared/SharedCard.vue';
 import RendererWorkflowService from '@/service/renderer/RendererWorkflowService';
+import { useSharedWorkflowStore } from '@/stores/sharedWorkflow';
+import { arraySorter } from '@/utils/helper';
+import { computed } from 'vue';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
 
 const props = defineProps({
   search: {
